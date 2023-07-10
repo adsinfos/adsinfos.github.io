2e28e53841f67e862ce03760e333f9e1effe3b12
usern4353 <user33333333@mail.com>
2023-07-10 10:10:19 -0400

    tiempos

--- src/app/shared/contratacion/contratacion.component.ts
+++ src/app/shared/contratacion/contratacion.component.ts
@@ -67,7 +67,7 @@ export class ContratacionComponent {
     }
     this.intervalId = setInterval(() => {
       this.count++;
-      if (this.count % 10 == 0) {
+      if (this.count % 100 == 0) {
         this.refreshDivs();
       }
     }, 1000);
--- src/app/shared/init/init.component.ts
+++ src/app/shared/init/init.component.ts
@@ -198,7 +198,7 @@ export class InitComponent {
           this.texto = "Ir a " + this.hostin;
           this.cuando = false;
         }
-        if (this.count % 3 == 0) {
+        if (this.count % 300 == 0) {
           this.refreshDivs();
         }
       }, 1000);
