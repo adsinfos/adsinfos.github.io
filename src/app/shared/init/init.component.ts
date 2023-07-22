83ad028304c29e45210e1b471e06c1dc34761a3a
usern4353 <user33333333@mail.com>
2023-07-21 21:01:37 -0400

    fix

--- src/app/shared/init/init.component.ts
+++ src/app/shared/init/init.component.ts
@@ -22,7 +22,7 @@ export class InitComponent {
   hostin: string = "";
   showInferior: boolean = true;
   showSuperior: boolean = true;
-  maximo: number = 5;
+  maximo: number = 15;
   cuando: boolean = false;
 
   hostsvalidos: string = "";
@@ -220,7 +220,7 @@ export class InitComponent {
           this.texto = "Ir a " + this.hostin;
           this.cuando = false;
         }
-        if (this.count % 20 == 0) {
+        if (this.count % 7 == 0) {
           this.refreshDivs();
         }
       }, 1000);
